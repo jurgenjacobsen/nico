d0fd050a712116884e41b0e9f0838d3a5a252ce4
Jurgen Jacobsen <jurgenjacobsen2005@gmail.com>
2021-08-29 18:40:08 -0300

    Removed test snippet from ready.ts

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,27 +1,11 @@
 import { EventOptions } from 'dsc.events'
 import { print } from '../utils/utils'
 import { Bot } from '../bot'
-import { Util } from 'dsc.levels';
 
 export const event: EventOptions = {
   name: 'ready',
   once: true,
   run: async (bot: Bot) => {
     print('Pronto! 😋');
-
-    setInterval(() => {
-      let voiceStates = bot.guilds.cache.get('465938334791893002')?.voiceStates.cache;
-      if(!voiceStates) return;
-
-      voiceStates?.forEach((vs, id) => {
-        if(!vs.channelId) return;
-        if(bot.config.voice.allowedStatsChannels.includes(vs.channelId)) {
-          bot.stats.users.update(vs.id, 'voice', 2);
-        };
-        if(bot.config.voice.allowedXPChannels.includes(vs.channelId)) {
-          bot.levels.update(vs.id, 'VOICE', Math.floor(Util.random(2, 7)));
-        };
-      });
-    }, 2 * 60 * 1000);
   },
 }
